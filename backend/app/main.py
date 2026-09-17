69413e1abb58c8f0826288d71914d6fa659fa308
Yugal Varshney <yugal.varshney0505@gmail.com>
2026-09-17 20:12:28 +0530

    change in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -112,6 +112,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://sagar-netra-82cmdu1ze-yugal-work.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
